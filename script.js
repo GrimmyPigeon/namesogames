052490184946b08b9e63f464c3479d383b9c1a9e
GrimmyPigeon <103783921+GrimmyPigeon@users.noreply.github.com>
2025-06-14 14:30:37 +0100

    completely changed it all, it was crap, now added a WIP screen

--- script.js
+++ script.js
@@ -1,46 +1,10 @@
-var myInput = document.getElementById("psw");
-var letter = document.getElementById("letter");
-var capital = document.getElementById("capital");
-var number = document.getElementById("number");
-var length = document.getElementById("length");
+const waveText = document.getElementById('wave-text');
+const text = waveText.textContent;
+waveText.textContent = '';
 
-myInput.onkeyup = function() {
-  // Validate lowercase letters
-  var lowerCaseLetters = /[a-z]/g;
-  if (myInput.value.match(lowerCaseLetters)) {
-    letter.classList.remove("invalid");
-    letter.classList.add("valid");
-  } else {
-    letter.classList.remove("valid");
-    letter.classList.add("invalid");
-  }
-
-  // Validate capital letters
-  var upperCaseLetters = /[A-Z]/g;
-  if (myInput.value.match(upperCaseLetters)) {
-    capital.classList.remove("invalid");
-    capital.classList.add("valid");
-  } else {
-    capital.classList.remove("valid");
-    capital.classList.add("invalid");
-  }
-
-  // Validate numbers
-  var numbers = /[0-9]/g;
-  if (myInput.value.match(numbers)) {
-    number.classList.remove("invalid");
-    number.classList.add("valid");
-  } else {
-    number.classList.remove("valid");
-    number.classList.add("invalid");
-  }
-
-  // Validate length
-  if (myInput.value.length >= 8) {
-    length.classList.remove("invalid");
-    length.classList.add("valid");
-  } else {
-    length.classList.remove("valid");
-    length.classList.add("invalid");
-  }
-};
+for(let i = 0; i < text.length; i++){
+  const span = document.createElement('span');
+  span.textContent = text[i];
+  span.style.animationDelay = `${i * 0.1}s`;
+  waveText.appendChild(span);
+}
